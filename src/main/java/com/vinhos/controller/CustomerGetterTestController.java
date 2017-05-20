8e092385ff16eccdb31d64a820cd7af3aa373d70
Lucas <lucasapetz5@hotmail.com>
2017-05-20 12:52:57 -0300

    test get

---
+++ src/main/java/com/vinhos/controller/CustomerGetterTestController.java
@@ -0,0 +1,28 @@
+package com.vinhos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.vinhos.model.Customer;
+import com.vinhos.repository.Customers;;
+
+@Controller
+public class CustomerGetterTestController {
+
+	@Autowired
+	private Customers customers;
+	
+	@GetMapping("/getcustomer")
+	public String vem(@RequestParam(value = "nome", defaultValue = "tret") String nome,
+			@RequestParam(value = "sobre", defaultValue = "fsz") String sobre) {
+		Customer cust = new Customer();
+		cust.setFirstname(nome);
+		cust.setLastname(sobre);
+		customers.save(cust);
+		return "DALE";
+	}
+}
