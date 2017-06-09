9fac610b24aa6b8da546e5d685e11456006feb12
Lucas <lucasapetz5@hotmail.com>
2017-06-09 15:27:19 -0300

    add sms sender

--- src/main/java/com/vinhos/config/SecurityConfig.java
+++ src/main/java/com/vinhos/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().
-		withUser("teste").password("teste").roles("PESQUISAR_VINHO");
+		withUser("admin").password("admin").roles("PESQUISAR_VINHO");
 	}
 	
 	@Override
